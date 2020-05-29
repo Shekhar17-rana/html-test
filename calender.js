04e11b910d58a420b5656284422c0cb13a1a1a36
Shekhar17-rana <64972508+Shekhar17-rana@users.noreply.github.com>
2020-05-29 15:42:47 +0530

    Update calender.js

--- calender.js
+++ calender.js
@@ -2,7 +2,7 @@ var dat = new Date();
 
 var months_arr=["January","Febuary","March","April","May","June","July","August","September","October","November","December"];
 
-var endDate = new Date(dat.getFullYear(),dat.getMonth()+1.0).getDate();
+var endDate = new Date(dat.getFullYear(),dat.getMonth()+1,0).getDate();
 
 document.getElementById("date_str").innerHTML = dat.toDateString();
 
