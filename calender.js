4e861d9c828c52af2b791906c70914227f771434
Shekhar17-rana <64972508+Shekhar17-rana@users.noreply.github.com>
2020-05-29 14:53:36 +0530

    Update calender.js

--- calender.js
+++ calender.js
@@ -1,3 +1,7 @@
 var dat = new Date();
 
 document.getElementById("date_str").innerHTML = dat.toDateString();
+
+var months_arr=["January","Febuary","March","April","May","June","July","August","September","October","November","December"];
+
+document.getElementById("month").innerHTML = months_arr[dat.getMonth()];
